1e6b8711c5ca74f9190d386bbf2776f1a5b3872d
YKim0172 <89658307+YKim0172@users.noreply.github.com>
2022-01-22 16:36:15 -0500

    Created logging in/creating account function

--- src/SecondaryServer.java
+++ src/SecondaryServer.java
@@ -5,8 +5,20 @@ import java.io.*;
 
 public class SecondaryServer implements Runnable {
     Socket socket;
+
+    /*
+     * sending and receiving strings
+     */
     BufferedReader bfr;
     PrintWriter pw;
+
+    /*
+     * sending and receiving objects
+     */
+
+    ObjectInputStream ois;
+    ObjectOutputStream oos;
+
     static ArrayList<Socket> allUsers;  //list of all clients that are connected to the server
     public SecondaryServer(Socket socket) {
         /*
@@ -18,12 +30,103 @@ public class SecondaryServer implements Runnable {
         }
         allUsers.add(this.socket);
 
+        try {
+            this.bfr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.pw = new PrintWriter(socket.getOutputStream());
+
+            this.ois = new ObjectInputStream(socket.getInputStream());
+            this.oos = new ObjectOutputStream(socket.getOutputStream());
+
+        } catch (Exception e) {
+
+        }
 
     }
 
     public void run() {
         while (true) {
+            try {
+                String action = bfr.readLine();
+
+                if(action.equals("createAccount")) {
+                    String name = bfr.readLine();
+                    String username = bfr.readLine();
+                    String password = bfr.readLine();
+
+                    if (accountValid(username)) {  //make account
+                        Farmer newFarmer = new Farmer(name, username, password);  //make new farmer object
+                        updateFarmerList(newFarmer);
+
+                        pw.write("accountMade");
+                        pw.println();
+                        pw.flush();
+
+                        oos.writeObject(newFarmer);
+                        oos.flush();
+                    } else {  //username already taken
+                        pw.write("accountNotMade");
+                        pw.println();
+                        pw.flush();
+                    }
+
+                } else if (action.equals("login")) {
+                    String username = bfr.readLine();
+                    String password = bfr.readLine();
+
+                    if (loggedIn(username, password) != null) {
+                        pw.write("loggedIn");
+                        pw.println();
+                        pw.flush();
+
+                        oos.writeObject(loggedIn(username, password));
+                        oos.flush();
+                    } else {
+                        pw.write("cantLogin");
+                        pw.println();
+                        pw.flush();
+                    }
+                }
+
+            } catch (Exception e) {
+
+            }
 
         }
+    }
+
+    public static ArrayList<Farmer> getFarmerList() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Farmers.txt"))) {
+            Object o = ois.readObject();
+            return (ArrayList<Farmer>) o;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static void updateFarmerList(Farmer farmer) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Farmers.txt"))) {
+            oos.writeObject(getFarmerList().add(farmer));
+        } catch (Exception e) {
+
+        }
+    }
+
+    public boolean accountValid(String username) {
+        for(Farmer f: getFarmerList()) {
+            if (f.getUsername().equals(username)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Farmer loggedIn(String username, String password) {
+
+        for (Farmer f : getFarmerList()) {
+            if (f.getUsername().equals(username) && f.getPassword().equals(password)) {
+                return f;
+            }
+        }
+        return null;
     }
 }
