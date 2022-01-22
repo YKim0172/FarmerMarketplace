8e8e88c867aa553a583e3bcff44330ce827efdf8
YKim0172 <89658307+YKim0172@users.noreply.github.com>
2022-01-21 22:11:59 -0500

    Made the farmer class (Farmer objects represent client)

--- src/Farmer.java
+++ src/Farmer.java
@@ -1,1 +1,11 @@
+public class Farmer {
+    private String name;  //name of farmer
+    private String username;  //username of farmer
+    private String password;  //password of farmer
 
+    public Farmer(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+}
