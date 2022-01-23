aefa0e545a91f0c726f06c0a6bf1cc2a84b10ae3
Yu Hyun Kim <89658307+YKim0172@users.noreply.github.com>
2022-01-22 22:59:30 -0500

    added constructors and getter methods

--- src/Selling.java
+++ src/Selling.java
@@ -1,8 +1,26 @@
 import java.io.*;
 
 public class Selling implements Serializable {
-    private String SellerUsername;
+    private String sellerUsername;
     private String nameOfItem;
     private double sellingPrice;
+
+    public Selling(String sellerUsername, String nameOfItem, double sellingPrice) {
+        this.sellerUsername = sellerUsername;
+        this.nameOfItem = nameOfItem;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getSellerUsername() {
+        return sellerUsername;
+    }
+
+    public String getNameOfItem() {
+        return nameOfItem;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
 
 }
