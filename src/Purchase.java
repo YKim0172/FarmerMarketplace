573b157244c73841e3aadb0a267fb0ed0462f1e3
Yu Hyun Kim <89658307+YKim0172@users.noreply.github.com>
2022-01-22 22:58:57 -0500

    added constructor / getter methods

--- src/Purchase.java
+++ src/Purchase.java
@@ -4,4 +4,22 @@ public class Purchase implements Serializable {
     private String usernameWhoBought;
     private String nameOfItem;
     private double pricePaid;
+
+    public Purchase(String usernameWhoBought, String nameOfItem, double pricePaid) {
+        this.usernameWhoBought = usernameWhoBought;
+        this.nameOfItem = nameOfItem;
+        this.pricePaid = pricePaid;
+    }
+
+    public String getUsernameWhoBought() {
+        return usernameWhoBought;
+    }
+
+    public String getNameOfItem() {
+        return nameOfItem;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
 }
