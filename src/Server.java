bae01d32bc5fa41280b7da8126836814308e29cb
Yu Hyun Kim <89658307+YKim0172@users.noreply.github.com>
2022-01-23 04:24:00 -0500

    removed unnecessary parts

--- src/Server.java
+++ src/Server.java
@@ -19,54 +19,15 @@ public class Server {
             }
         }
 
-
-
-
-        /*
-         * Creates the PurchaseHistory.txt file and stores the ArrayList of Purchase objects if
-         * the text file wasn't already created.
-         *
-         * By storing Purchase objects in the file, we can ensure all data is saved regarding farmers'
-         * purchasing history.
-         */
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("PurchaseHistory.txt"))) {
-        } catch (Exception a) {
-            ArrayList<Purchase> purchaseArrayList = new ArrayList<>();
-            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("PurchaseHistory.txt"))) {
-                oos.writeObject(purchaseArrayList);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        /*
-         * Creates the SellingHistory.txt file and stores the ArrayList of Selling objects if
-         * the text file isn't already created.
-         *
-         * By storing Selling objects in the file, we can ensure all data is saved regarding farmers'
-         * selling history
-         */
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("SellingHistory.txt"))) {
-        } catch (Exception a) {
-            ArrayList<Selling> sellingArrayList = new ArrayList<>();
-            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("SellingHistory.txt"))) {
-                oos.writeObject(sellingArrayList);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Sales.txt"))) {
         } catch (Exception a) {
-            ArrayList<Selling> salesArrayList = new ArrayList<>();
+            ArrayList<Sales> salesArrayList = new ArrayList<>();
             try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Sales.txt"))) {
                 oos.writeObject(salesArrayList);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-
-
 
         while (true) {
             System.out.println("accepting input");
